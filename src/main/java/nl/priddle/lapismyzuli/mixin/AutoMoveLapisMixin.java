78e69e23b048b76e9f1f304a6168ee6b0bc05ba4
Niels van Dijk <niels@nielsvandijk.eu>
2024-12-13 12:23:26 +0100

    Refactor + add auto refill

--- src/main/java/nl/priddle/lapismyzuli/mixin/AutoMoveLapisMixin.java
+++ src/main/java/nl/priddle/lapismyzuli/mixin/AutoMoveLapisMixin.java
@@ -2,36 +2,45 @@ package nl.priddle.lapismyzuli.mixin;
 
 import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Items;
-import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.EnchantmentScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(HandledScreen.class)
-public abstract class AutoMoveLapisMixin {
+@Mixin(EnchantmentScreen.class)
+@SuppressWarnings("unused")
+public abstract class AutoMoveLapisMixin extends HandledScreen<EnchantmentScreenHandler> {
     private static final int ENCHANTMENT_LAPIS_SLOT = 1;
     private static final int INVENTORY_START_INDEX = 2;
 
-    @Shadow
-    public ScreenHandler handler;
+    public AutoMoveLapisMixin(EnchantmentScreenHandler handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+    }
 
-    @Inject(at = @At("TAIL"), method = "init")
-    private void init(CallbackInfo info) {
-        // Check if this is the EnchantmentScreen.class
-        if (((Object) this).getClass() != EnchantmentScreen.class) {
-            return;
-        }
+    @Inject(at = @At("TAIL"), method = "init()V")
+    public void init(CallbackInfo info) {
+        moveLapisToEnchantmentTable(this.handler.slots);
+    }
 
+    @Inject(at = @At(value = "RETURN", ordinal = 0), method = "mouseClicked(DDI)Z")
+    public void mouseClicked(CallbackInfoReturnable<Boolean> info) {
+        moveLapisToEnchantmentTable(this.handler.slots);
+    }
+
+    private void moveLapisToEnchantmentTable(DefaultedList<Slot> slots) {
         // Get the lapis slot in the enchantment table.
-        Slot lapisSlot = handler.getSlot(ENCHANTMENT_LAPIS_SLOT);
+        Slot lapisSlot = slots.get(ENCHANTMENT_LAPIS_SLOT);
 
         // Find all lapis in the inventory and move them to the lapis slot.
-        for (int i = INVENTORY_START_INDEX; i < handler.slots.size(); i++) {
+        for (int i = INVENTORY_START_INDEX; i < slots.size(); i++) {
 
             // Check if the lapis slot is full.
             boolean isFull = lapisSlot.hasStack() && lapisSlot.getStack().getCount() == lapisSlot.getMaxItemCount();
@@ -40,15 +49,12 @@ public abstract class AutoMoveLapisMixin {
             }
 
             // Move lapis to the lapis slot.
-            Slot slot = handler.getSlot(i);
+            Slot slot = slots.get(i);
             if (slot.hasStack() && slot.getStack().isOf(Items.LAPIS_LAZULI)) {
-                onMouseClick(slot, slot.id, 0, SlotActionType.PICKUP);
-                onMouseClick(lapisSlot, lapisSlot.id, 0, SlotActionType.PICKUP);
-                onMouseClick(slot, slot.id, 0, SlotActionType.PICKUP);
+                this.onMouseClick(slot, slot.id, 0, SlotActionType.PICKUP);
+                this.onMouseClick(lapisSlot, lapisSlot.id, 0, SlotActionType.PICKUP);
+                this.onMouseClick(slot, slot.id, 0, SlotActionType.PICKUP);
             }
         }
     }
-
-    @Shadow
-    protected abstract void onMouseClick(Slot slot, int slotId, int button, SlotActionType actionType);
 }
